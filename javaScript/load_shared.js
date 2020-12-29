8f20b551cc897a6784016936a1863efd28150343
zhihuiyang73 <59304470+zhihuiyang73@users.noreply.github.com>
2020-12-29 10:57:18 -0600

    initial

---
+++ javaScript/load_shared.js
@@ -0,0 +1,12 @@
+
+
+    document.getElementById("headerTitle").innerHTML = "<h2><a href='index.html'> DW Math Interest Group</a></h2>"
+                                                + "<p>This is the Homepage of the site. Write something here...</p>";
+  
+
+    document.getElementById("navigation").innerHTML = "<ul><li><a href='01022021.html'>Jan 02, 2021</a></li>"
+                                + "<li><a href='01052021.html'>Jan 05, 2021</a></li>"
+                                + "<li><a href='01092021.html'>Jan 09, 2021</a></li></ul>";
+
+
+    document.getElementById("myFooter").innerHTML = " <p>any thought what we should do here? ... </p>";
