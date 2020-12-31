658f28691645a2eaf9d213e8ebc031d568c09ee7
notrlyrainy <69054170+notrlyrainy@users.noreply.github.com>
2020-12-31 15:45:44 -0600

    added stuff

--- javaScript/load_shared.js
+++ javaScript/load_shared.js
@@ -1,12 +1,11 @@
 
 
-    document.getElementById("headerTitle").innerHTML = "<h2><a href='index.html'> DW Math Interest Group</a></h2>"
+    document.getElementById("headerTitle").innerHTML = "<h2><a href='index.html'> <img id='templogo' alt='DW Interest Group' src='images/logo.jpg'</img></a></h2>"
                                                 + "<p>This is the homepage for the best math group ever.</p>";
   
 
-    document.getElementById("navigation").innerHTML = "<ul><li><a href='01022021.html'>Jan 02, 2021</a></li>"
-                                + "<li><a href='01052021.html'>Jan 05, 2021</a></li>"
-                                + "<li><a href='01092021.html'>Jan 09, 2021</a></li></ul>";
+    document.getElementById("navigation").innerHTML = "<li><a href='schedule.html'>Schedule</a></li></ul>" 
+                                + "<li><a href='edulinks.html'>Educational links</a></li></ul>"
 
 
     document.getElementById("myFooter").innerHTML = " <p>Explore the plethora of math skills we hone it our sessions.</p>";
